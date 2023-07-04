f245b547866bfeafcebf9b81ee81783050f0879e
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-07-04 13:27:41 -0300

    Criando componentes de Titulo e Paragrafo

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/fde95.png" alt="" />
+    <Titulo fontSize={20}>Felipe Espinoza</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre</Titulo>
+    <Paragrafo tipo="principal">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Error sed nemo
+      sit odit perspiciatis nesciunt. Voluptates cupiditate natus dolore error
+      facilis placeat porro, aliquid sit quasi rem quaerat non mollitia?
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
