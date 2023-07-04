55bb54fe96cadce5e53f1fe5579954a05719d3cb
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-07-04 13:50:08 -0300

    Construindo estrutura e a seção Sobre Mim

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -9,6 +10,10 @@ const Sobre = () => (
       sit odit perspiciatis nesciunt. Voluptates cupiditate natus dolore error
       facilis placeat porro, aliquid sit quasi rem quaerat non mollitia?
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=fde95&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=fde95&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
