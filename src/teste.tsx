cc6c72658d1c4671c3323d2b96f961d09bdce840
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-07-04 10:35:48 -0300

    configurando o ambiente de desenvolvimento

---
+++ src/teste.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+type BotaoProps = {
+  principal: boolean
+  fontSize?: string
+}
+
+const Botao = styled.button<BotaoProps>`
+  background-color: ${(props) => (props.principal ? 'green' : 'red')};
+  font-size: ${(props) => props.fontSize || '16px'};
+  cursor: pointer;
+`
+const BotaoPerigo = styled(Botao)`
+  background-color: yellow;
+  font-weight: bold;
+
+  span {
+    text-decoration: line-through;
+  }
+`
+
+function Teste() {
+  return (
+    <>
+      <Botao fontSize="18px" principal>
+        Enviar
+      </Botao>
+      <Botao fontSize="14px" principal={false}>
+        Cancelar
+      </Botao>
+      <BotaoPerigo as="a" principal>
+        <span>Não clique aqui</span>
+      </BotaoPerigo>
+    </>
+  )
+}
+
+export default Teste
