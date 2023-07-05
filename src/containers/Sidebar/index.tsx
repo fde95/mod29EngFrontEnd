8511d8256baa73ecac3d4e79bd3692b1bc0d3cc6
Felipe Espinoza <domingues.espinoza@outlook.com>
2023-07-05 14:09:13 -0300

    Estilizando a Sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/fde95.png" alt="" />
-    <Titulo fontSize={20}>Felipe Espinoza</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Felipe Espinoza</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        fde95
+      </Paragrafo>
+      <Descricao fontSize={12} tipo="principal">
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
